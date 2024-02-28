bea3677676882d27354350e50a89adb9e7cc55ad
Edgar Chavez <erc1846@mavs.uta.edu>
2024-02-28 17:37:08 -0600

    Update Toy Search Engine.py
    Applied the Google Python Style method to my functions

--- Toy Search Engine.py
+++ Toy Search Engine.py
@@ -7,6 +7,16 @@ import math
 
 
 def lower_case():
+    """
+    This function reads the inaugural addresses from the US_Inaugural_Addresses folder and 
+    returns a dictionary withe the filename as the key and the content of the file as the value. 
+
+    Args: 
+        None.
+
+    Returns:
+        inaugural_addresses: A dictionary with the filename as the key and the text of the file as the value.
+    """
     corpusroot = './US_Inaugural_Addresses'
     inaugural_addresses = {}
     for filename in os.listdir(corpusroot):
@@ -19,6 +29,15 @@ def lower_case():
     return inaugural_addresses
 
 def tokenize(inaugural_addresses):
+    """
+    This function takes the dictionary of inaugural addresses and tokenizes the text of each address. 
+
+    Args: 
+        inaugural_addresses: Dictionary of inaugural addresses
+
+    Returns:
+        inaugural_tokens: A dictionary with the tokenized text of each address. 
+    """
     tokenizer = RegexpTokenizer(r'[a-zA-z]+')
     inaugural_tokens = {}
     for filename, text in inaugural_addresses.items():
@@ -27,6 +46,15 @@ def tokenize(inaugural_addresses):
     return inaugural_tokens
 
 def remove_stopwords(inaugural_tokens):
+    """
+    This function takes the dictionary of tokenized inaugural_addresses and removes the stop words.
+
+    Args:
+        inaugural_tokens: Dictionary of tokenized inaugural addresses
+    
+    Returns:
+        inaugural_tokens_nostopwords: A dictionary with the tokenized text of each address without the stop words. 
+    """
     english_stopwords = stopwords.words('english')
     inaugural_tokens_nostopwords = {}
     for filename, tokens in inaugural_tokens.items():
@@ -37,6 +65,15 @@ def remove_stopwords(inaugural_tokens):
     return inaugural_tokens_nostopwords
 
 def apply_stemmer_docs(inaugural_tokens_nostopwords):
+    """
+    This function takes the dictionary of tokenized inaugural_addresses without stop words and applies the stemmer to each token.
+
+    Args:
+        inaugural_tokens_nostopwords: Dictionary of tokenized inaugural addresses without stop words
+    
+    Returns:
+        inaugural_stemmed: Dictionary with stemmed tokens of each address. 
+    """
     stemmer = PorterStemmer()
     inaugural_stemmed = {}
     for filename, tokens in inaugural_tokens_nostopwords.items():
@@ -46,10 +83,28 @@ def apply_stemmer_docs(inaugural_tokens_nostopwords):
     return inaugural_stemmed
 
 def apply_stemmer_token(token):
+    """
+    Function to apply stemmer to a single token. Used for queries. 
+    
+    Args: 
+        token: A single token (word)
+    
+    Returns:
+        stemmer.stem(token): The stemmed token.
+    """
     stemmer = PorterStemmer()
     return stemmer.stem(token)
 
 def get_raw_term_frequency(inaugural_stemmed):
+    """
+    Function takes dictionary of stemmed tokens and finds the raw term frequency of each token in each doc. 
+
+    Args:
+        inaugural_stemmed: Dictionary with stemmed tokens of each address. 
+    
+    Returns:
+        raw_term_frequency: Dictionary with the raw term frequency of each token in each doc. 
+    """
     raw_term_frequency = {}
     for filename, tokens in inaugural_stemmed.items():
         raw_term_frequency[filename] = {}
@@ -61,6 +116,15 @@ def get_raw_term_frequency(inaugural_stemmed):
     return raw_term_frequency
 
 def getidf(token):
+    """
+    Function calculates the inverse document frequency of a token in the corpus. 
+
+    Args: 
+        token: A single token (word)
+    
+    Returns: 
+        inverse_document_frequency: The inverse document frequency of the token in the corpus. 
+    """
     stemmed_token = apply_stemmer_token(token)
     total_docs = len(inaugural_stemmed)
 
@@ -74,6 +138,16 @@ def getidf(token):
     return inverse_document_frequency
 
 def normalize_values(inaugural_stemmed, raw_term_frequency):
+    """
+    Function that finds the magnitude of the tf-idf values for each doc in the corpus.
+
+    Args: 
+        inaugural_stemmed: Dictionary with stemmed tokens of each address. 
+        raw_term_frequency: Dictionary with raw term frequency of each token in each doc. 
+    
+    Returns:
+        normalized_values: Dictionary with the magnitudes of the tf-idf values for each doc. 
+    """
     normalized_values = {}
     for filename, tokens in inaugural_stemmed.items():
         magnitude = 0
@@ -86,6 +160,16 @@ def normalize_values(inaugural_stemmed, raw_term_frequency):
     return normalized_values
 
 def getweight(filename, token):
+    """
+    Function that calculates the tf-idf value of a token in a document.
+    
+    Args:
+        filename: The  name of the doc.
+        token: A single token (word)
+    
+    Returns: 
+        term_frequency_inverse_document_frequency / normalized_values[filename]: The normalized tf-idf value of the token in the doc. 
+    """
     stemmed_token = apply_stemmer_token(token)
     term_frequency = raw_term_frequency[filename].get(stemmed_token, 0)
     if term_frequency == 0:
@@ -95,6 +179,15 @@ def getweight(filename, token):
     return term_frequency_inverse_document_frequency / normalized_values[filename]
 
 def query(qstring):
+    """
+    Function that takes a query string and returns the doc with the highest similarity score. Following the ltc.lnc model.
+    
+    Args:
+        qstring: A string with the query
+    
+    Returns:
+        (best_doc, best_score): A tuple with the best doc and the similarity score.
+    """
     tokenizer = RegexpTokenizer(r'[a-zA-z]+')
     query_tokens = tokenizer.tokenize(qstring.lower())
     english_stopwords = stopwords.words('english')
